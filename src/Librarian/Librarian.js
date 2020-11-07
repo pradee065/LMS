1c17fb9876d639a3dd2be3d5d1734290e1c3c629
pradee065 <pradeepbharthi065@gmail.com>
2020-11-07 10:37:24 +0530

    Git intial setup and inital design is in progress

--- src/Librarian/Librarian.js
+++ src/Librarian/Librarian.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './Librarian.css'
 
 const Librarian = () => {
    return (
-       <p>Lirarian intial setup is done here</p>
+       <div className="librarain-container">
+        <h1>Librarian Member Login </h1>
+        <form>
+          <label for="uname"><b>Username</b></label>
+          <input type="text"></input>
+
+          <label for="psw"><b>Password</b></label>
+          <input type="password"></input>
+
+          <button type="submit">Login</button>
+        </form>   
+
+
+       </div>
    )
 }
 
