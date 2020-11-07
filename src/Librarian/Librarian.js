d45f4cec2c05127c8f53f29312b6745e7558c13e
pradee065 <pradeepbharthi065@gmail.com>
2020-11-07 12:11:02 +0530

    working on backend api

--- src/Librarian/Librarian.js
+++ src/Librarian/Librarian.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import './Librarian.css'
+import './Librarian.css';
+import axios from 'axios';
 
 const Librarian = (props) => {
    return (
        <div className="librarain-container">
         <h2>Librarian Member Login </h2>
-        <form>
+        <form onSubmit ={(e) => login(e)}>
           <label for="uname"><b>Username</b></label>
-          <input type="text"></input>
+          <input type="text" id="user-input"></input>
 
           <label for="psw"><b>Password</b></label>
-          <input type="password"></input>
+          <input type="password" id="user-password"></input>
 
           <button type="submit">Login</button>
         </form>   
@@ -18,6 +19,21 @@ const Librarian = (props) => {
 
        </div>
    )
+}
+
+function login(e){
+    e.preventDefault();
+    let request = {
+        email: document.getElementById('user-input').value,
+        password: document.getElementById('user-password').value
+    }
+    axios.post('http://localhost:3000/login', request)
+    .then( resp => {
+        alert(resp.data.message);
+    })
+    .catch( err => {
+        console.log(err);
+    })
 }
 
 
